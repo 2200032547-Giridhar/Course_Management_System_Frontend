fea4346bc5b1a555a0fcf2106e6f16f54dd2d967
2200032547-Giridhar <161588814+2200032547-Giridhar@users.noreply.github.com>
2024-12-30 15:04:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,23 +25,23 @@ function App() {
         <Router>
             <Routes>
                 {/* Public Routes */}
-                <Route path="https://coursemanagementsystembackend-production.up.railway.app/api/v1/" element={<LoginComponent />} />
-                <Route path="https://coursemanagementsystembackend-production.up.railway.app/api/v1/register" element={<RegisterComponent />} />
-                <Route path="https://coursemanagementsystembackend-production.up.railway.app/api/v1/login" element={<LoginComponent />} />
-                <Route path="https://coursemanagementsystembackend-production.up.railway.app/api/v1/student-dashboard/:id" element={<StudentDashboard />} />
-                <Route path="https://coursemanagementsystembackend-production.up.railway.app/api/v1/faculty-dashboard/:id" element={<FacultyDashboard />} />
-                <Route path="https://coursemanagementsystembackend-production.up.railway.app/api/v1/admin-dashboard/:id" element={<AdminDashboard />} />
-                <Route path="https://coursemanagementsystembackend-production.up.railway.app/api/v1/students-list/:id" element={<StudentsList />} />
-                <Route path="https://coursemanagementsystembackend-production.up.railway.app/api/v1/courses/:id" element={<Courses />}  />
-                <Route path="https://coursemanagementsystembackend-production.up.railway.app/api/v1/course-list/:id"element={<CourseList />}  />
-                <Route path="https://coursemanagementsystembackend-production.up.railway.app/api/v1/create-course"element={<AddCourse />}  />
-                <Route path="https://coursemanagementsystembackend-production.up.railway.app/api/v1/update-course/:courseId" element={<UpdateCourse />} />
-                <Route path="https://coursemanagementsystembackend-production.up.railway.app/api/v1/courses/:id/:courseId" element={<CourseDetails />} />
-                <Route path="https://coursemanagementsystembackend-production.up.railway.app/api/v1/:id/enrolled-courses" element={<EnrolledCourses/>}/>
-                <Route path="https://coursemanagementsystembackend-production.up.railway.app/api/v1/Help/:id" element={<Help/>}/>
-                <Route path="https://coursemanagementsystembackend-production.up.railway.app/api/v1/settings/:id" element={<Settings/>}/>
-                <Route path="https://coursemanagementsystembackend-production.up.railway.app/api/v1/help1/:id" element={<Help1/>}/>
-                <Route path="https://coursemanagementsystembackend-production.up.railway.app/api/v1/settings1/:id" element={<Settings1/>}/>
+                <Route path="/" element={<LoginComponent />} />
+                <Route path="/register" element={<RegisterComponent />} />
+                <Route path="/login" element={<LoginComponent />} />
+                <Route path="/student-dashboard/:id" element={<StudentDashboard />} />
+                <Route path="/faculty-dashboard/:id" element={<FacultyDashboard />} />
+                <Route path="/admin-dashboard/:id" element={<AdminDashboard />} />
+                <Route path="/students-list/:id" element={<StudentsList />} />
+                <Route path="/courses/:id" element={<Courses />}  />
+                <Route path="course-list/:id"element={<CourseList />}  />
+                <Route path="/create-course"element={<AddCourse />}  />
+                <Route path="/update-course/:courseId" element={<UpdateCourse />} />
+                <Route path="/courses/:id/:courseId" element={<CourseDetails />} />
+                <Route path="/:id/enrolled-courses" element={<EnrolledCourses/>}/>
+                <Route path="Help/:id" element={<Help/>}/>
+                <Route path="/settings/:id" element={<Settings/>}/>
+                <Route path="help1/:id" element={<Help1/>}/>
+                <Route path="/settings1/:id" element={<Settings1/>}/>
             </Routes>
         </Router>
     );
