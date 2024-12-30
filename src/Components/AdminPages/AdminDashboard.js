80cf075dd31f07d5f97d43b3ad2343f00cea4fac
2200032547-Giridhar <161588814+2200032547-Giridhar@users.noreply.github.com>
2024-12-30 14:47:04 +0530

    Update AdminDashboard.js

--- src/Components/AdminPages/AdminDashboard.js
+++ src/Components/AdminPages/AdminDashboard.js
@@ -90,37 +90,37 @@ useEffect(() => {
         </div>
         <ul>
           <li>
-            <a href={`/admin-dashboard/${id}`}>
+            <a href={`https://coursemanagementsystembackend-production.up.railway.app/api/v1/admin-dashboard/${id}`}>
               <FontAwesomeIcon icon={faHome} />
               <span className="nav-item">Home</span>
             </a>
           </li>
           <li>
-            <a href={`/students-list/${id}`}>
+            <a href={`https://coursemanagementsystembackend-production.up.railway.app/api/v1/students-list/${id}`}>
               <FontAwesomeIcon icon={faUsersCog} />
               <span className="nav-item">Users List</span>
             </a>
           </li>
           <li>
-            <a href={`/course-list/${id}`}>
+            <a href={`https://coursemanagementsystembackend-production.up.railway.app/api/v1/course-list/${id}`}>
               <FontAwesomeIcon icon={faGraduationCap} />
               <span className="nav-item">Courses List</span>
             </a>
           </li>
           <li>
-            <a href={`/settings1/${id}`}>
+            <a href={`https://coursemanagementsystembackend-production.up.railway.app/api/v1/settings1/${id}`}>
               <FontAwesomeIcon icon={faCog} />
               <span className="nav-item">Profile</span>
             </a>
           </li>
           <li>
-            <a href={`/help1/${id}`}>
+            <a href={`https://coursemanagementsystembackend-production.up.railway.app/api/v1/help1/${id}`}>
               <FontAwesomeIcon icon={faQuestionCircle} />
               <span className="nav-item">Help</span>
             </a>
           </li>
           <li>
-            <a href="/login">
+            <a href="https://coursemanagementsystembackend-production.up.railway.app/api/v1/login">
               <FontAwesomeIcon icon={faSignOutAlt} />
               <span>Logout</span>
             </a>
