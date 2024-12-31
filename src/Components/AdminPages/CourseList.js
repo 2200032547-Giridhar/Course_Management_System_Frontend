148cd575d1045982b418e5c4d6fb39a2759b8356
2200032547-Giridhar <161588814+2200032547-Giridhar@users.noreply.github.com>
2024-12-31 17:04:16 +0530

    Update CourseList.js

--- src/Components/AdminPages/CourseList.js
+++ src/Components/AdminPages/CourseList.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignOutAlt, faHome, faGraduationCap, faUsersCog, faCog, faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
 import './CourseList.css';
-import CourseUpdateModal from './UpdateCourse'; // Import the modal component
+import CourseUpdateModal from './UpdateCourse';
 import AddCourseModal from './AddCourse';
 import { useNavigate, useParams } from "react-router-dom";
 
@@ -14,41 +14,37 @@ const CourseList = () => {
   const [selectedCourseId, setSelectedCourseId] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isAddCourseModalOpen, setIsAddCourseModalOpen] = useState(false);
-  const [user, setUser] = useState(null); 
-  const { id } = useParams(); // Access user ID from the URL
+  const [user, setUser] = useState(null);
+  const { id } = useParams();
   const [studentDetails, setStudentDetails] = useState({});
   const navigate = useNavigate();
   const [enrolledStudents, setEnrolledStudents] = useState([]);
   const [isStudentsModalOpen, setIsStudentsModalOpen] = useState(false);
 
-
   useEffect(() => {
     fetch(`https://coursemanagementsystembackend-production.up.railway.app/api/v1/users/${id}`)
-        .then((response) => response.json())
-        .then((data) => setStudentDetails(data))
-        .catch((error) => console.error(error));
-}, [id]);
-
-useEffect(() => {
-  const storedUser = JSON.parse(localStorage.getItem("user"));
-  if (storedUser) {
-      setUser(storedUser);  // Set the user info to state
-      // Redirect to the dashboard with student ID in the URL (assuming the student ID is stored in user.id)
-      navigate(`/course-list/${id}`);
-  } else {
-      navigate("/login");  // If no user, redirect to login page
-  }
-}, [navigate, id]);
-
+      .then((response) => response.json())
+      .then((data) => setStudentDetails(data))
+      .catch((error) => console.error(error));
+  }, [id]);
 
   useEffect(() => {
-    // Fetch all courses from the backend API
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    if (storedUser) {
+      setUser(storedUser);
+      navigate(`/course-list/${id}`);
+    } else {
+      navigate("/login");
+    }
+  }, [navigate, id]);
+
+  useEffect(() => {
     axios
       .get('https://coursemanagementsystembackend-production.up.railway.app/api/v1/courses/course-list')
       .then((response) => {
-        setCourses(response.data); // Set the courses data
+        setCourses(response.data);
       })
-      .catch((error) => {
+      .catch(() => {
         setMessage('Error fetching courses');
       });
   }, []);
@@ -56,11 +52,11 @@ useEffect(() => {
   const handleDelete = (courseId) => {
     axios
       .delete(`https://coursemanagementsystembackend-production.up.railway.app/api/v1/courses/delete/${courseId}`)
-      .then((response) => {
+      .then(() => {
         setCourses(courses.filter((course) => course.courseId !== courseId));
         setMessage('Course deleted successfully!');
       })
-      .catch((error) => {
+      .catch(() => {
         setMessage('Error deleting course');
       });
   };
@@ -92,7 +88,6 @@ useEffect(() => {
   };
 
   const handleCourseUpdate = (updatedCourse) => {
-    // Update the course in the list after it's updated
     setCourses(
       courses.map((course) =>
         course.courseId === updatedCourse.courseId ? updatedCourse : course
@@ -101,14 +96,13 @@ useEffect(() => {
   };
 
   const openStudentListModal = (courseId) => {
-    // Fetch the students enrolled in the course
     axios
       .get(`https://coursemanagementsystembackend-production.up.railway.app/api/v1/courses/${courseId}/students`)
       .then((response) => {
-        setEnrolledStudents(response.data); // Set enrolled students
-        setIsStudentsModalOpen(true); // Open the modal to show students
+        setEnrolledStudents(response.data);
+        setIsStudentsModalOpen(true);
       })
-      .catch((error) => {
+      .catch(() => {
         setMessage('Error fetching enrolled students');
       });
   };
@@ -123,7 +117,7 @@ useEffect(() => {
       <div className="sidebar">
         <div className="logo">Coding Courses</div>
         <ul>
-        <li><a href={`/admin-dashboard/${id}`}><FontAwesomeIcon icon={faHome} /> Home</a></li>
+          <li><a href={`/admin-dashboard/${id}`}><FontAwesomeIcon icon={faHome} /> Home</a></li>
           <li><a href={`/students-list/${id}`}><FontAwesomeIcon icon={faUsersCog} /> Users List</a></li>
           <li><a href={`/course-list/${id}`}><FontAwesomeIcon icon={faGraduationCap} /> Courses List</a></li>
           <li><a href={`/settings1/${id}`}><FontAwesomeIcon icon={faCog} /> Profile</a></li>
@@ -131,14 +125,14 @@ useEffect(() => {
           <li><a href="/login"><FontAwesomeIcon icon={faSignOutAlt} /> Logout</a></li>
         </ul>
         <footer>
-                    <p>&copy; 2024 Uppada Giridhar. All Rights Reserved.</p>
-                    <div className="social-icons">
-                        <a href="https://www.facebook.com/sanjay.uppada.7/" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook"></i></a>
-                        <a href="https://x.com/giridhar_uppada" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i></a>
-                        <a href="https://www.linkedin.com/in/uppada-giridhar/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
-                        <a href="https://www.instagram.com/__sanjay__uppada__/?__pwa=1" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a>
-                    </div>
-                </footer>
+          <p>&copy; 2024 Uppada Giridhar. All Rights Reserved.</p>
+          <div className="social-icons">
+            <a href="https://www.facebook.com/sanjay.uppada.7/" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook"></i></a>
+            <a href="https://x.com/giridhar_uppada" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i></a>
+            <a href="https://www.linkedin.com/in/uppada-giridhar/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
+            <a href="https://www.instagram.com/__sanjay__uppada__/?__pwa=1" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a>
+          </div>
+        </footer>
       </div>
 
       <div className="main-content">
@@ -156,11 +150,9 @@ useEffect(() => {
           </div>
           <h2>Welcome to the Admin Dashboard</h2>
           <div className="username">
-          {user && (
-                        <div className="username">
-                            <p>Hello, <strong> {studentDetails.firstName} {studentDetails.lastName}</strong></p>  {/* Displaying username */}
-                        </div>
-                    )}
+            {user && (
+              <p>Hello, <strong>{studentDetails.firstName} {studentDetails.lastName}</strong></p>
+            )}
           </div>
         </header>
 
@@ -192,36 +184,37 @@ useEffect(() => {
         {isAddCourseModalOpen && (
           <AddCourseModal onClose={closeAddCourseModal} />
         )}
-{isStudentsModalOpen && (
-  <div className="students-modal">
-    <div className="students-modal-container">
-      <h3>Enrolled Students</h3>
-      {enrolledStudents.length === 0 ? (
-        <p>No students enrolled in this course.</p>
-      ) : (
-        <table className="students-table">
-          <thead>
-            <tr>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Email</th>
-            </tr>
-          </thead>
-          <tbody>
-            {enrolledStudents.map((student) => (
-              <tr key={student.id}>
-                <td>{student.firstName}</td>
-                <td>{student.lastName}</td>
-                <td>{student.email}</td> {/* Assuming you want to display email */}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-      <button onClick={closeStudentListModal}>Close</button>
-    </div>
-  </div>
-)}
+
+        {isStudentsModalOpen && (
+          <div className="students-modal">
+            <div className="students-modal-container">
+              <h3>Enrolled Students</h3>
+              {enrolledStudents.length === 0 ? (
+                <p>No students enrolled in this course.</p>
+              ) : (
+                <table className="students-table">
+                  <thead>
+                    <tr>
+                      <th>First Name</th>
+                      <th>Last Name</th>
+                      <th>Email</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {enrolledStudents.map((student) => (
+                      <tr key={student.id}>
+                        <td>{student.firstName}</td>
+                        <td>{student.lastName}</td>
+                        <td>{student.email}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              )}
+              <button onClick={closeStudentListModal}>Close</button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
