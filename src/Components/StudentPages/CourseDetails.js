d76445a4dca284513d78d2cec58e95b1c0083fa5
2200032547-Giridhar <161588814+2200032547-Giridhar@users.noreply.github.com>
2024-12-31 18:31:53 +0530

    Update CourseDetails.js

--- src/Components/StudentPages/CourseDetails.js
+++ src/Components/StudentPages/CourseDetails.js
@@ -108,14 +108,50 @@ function CourseDetails({ userId }) {
                     <div className="logo">Coding Courses</div>
                 </div>
                 <ul>
-                    <li><a href={`/student-dashboard/${id}`}><FontAwesomeIcon icon={faHome} /><span>Home</span></a></li>
-                    <li><a href={`/courses/${id}`}><FontAwesomeIcon icon={faGraduationCap} /><span>Courses</span></a></li>
-                    <li><a href={`/${id}/enrolled-courses`}><FontAwesomeIcon icon={faChartBar} /><span>My Courses</span></a></li>
-                    <li><a href="tasks"><FontAwesomeIcon icon={faTasks} /><span>Tasks</span></a></li>
-                    <li><a href={`/settings/${id}`}><FontAwesomeIcon icon={faCog} /><span>Profile</span></a></li>
-                    <li><a href={`/Help/${id}`}><FontAwesomeIcon icon={faQuestionCircle} /><span>Help</span></a></li>
-                    <li><a href="/login"><FontAwesomeIcon icon={faSignOutAlt} /><span>Logout</span></a></li>
-                </ul>
+  <li>
+    <Link to={`/student-dashboard/${id}`}>
+      <FontAwesomeIcon icon={faHome} />
+      <span>Home</span>
+    </Link>
+  </li>
+  <li>
+    <Link to={`/courses/${id}`}>
+      <FontAwesomeIcon icon={faGraduationCap} />
+      <span>Courses</span>
+    </Link>
+  </li>
+  <li>
+    <Link to={`/${id}/enrolled-courses`}>
+      <FontAwesomeIcon icon={faChartBar} />
+      <span>My Courses</span>
+    </Link>
+  </li>
+  <li>
+    <Link to="/tasks">
+      <FontAwesomeIcon icon={faTasks} />
+      <span>Tasks</span>
+    </Link>
+  </li>
+  <li>
+    <Link to={`/settings/${id}`}>
+      <FontAwesomeIcon icon={faCog} />
+      <span>Profile</span>
+    </Link>
+  </li>
+  <li>
+    <Link to={`/Help/${id}`}>
+      <FontAwesomeIcon icon={faQuestionCircle} />
+      <span>Help</span>
+    </Link>
+  </li>
+  <li>
+    <Link to="/login">
+      <FontAwesomeIcon icon={faSignOutAlt} />
+      <span>Logout</span>
+    </Link>
+  </li>
+</ul>
+
                 <footer>
                     <p>&copy; 2024 Uppada Giridhar. All Rights Reserved.</p>
                     <div className="social-icons">
