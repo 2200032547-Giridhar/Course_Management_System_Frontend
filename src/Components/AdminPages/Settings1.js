119e8ca11ff1e3d8a665d4b4179a201739c87cdf
2200032547-Giridhar <161588814+2200032547-Giridhar@users.noreply.github.com>
2024-12-31 17:07:25 +0530

    Update Settings1.js

--- src/Components/AdminPages/Settings1.js
+++ src/Components/AdminPages/Settings1.js
@@ -1,7 +1,16 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSignOutAlt, faArrowLeft, faSave, faHome, faGraduationCap, faUsersCog, faCog, faQuestionCircle } from "@fortawesome/free-solid-svg-icons";
+import {
+    faSignOutAlt,
+    faArrowLeft,
+    faSave,
+    faHome,
+    faGraduationCap,
+    faUsersCog,
+    faCog,
+    faQuestionCircle,
+} from "@fortawesome/free-solid-svg-icons";
 import "./Settings1.css";
 import { useNavigate, useParams } from "react-router-dom";
 
@@ -21,7 +30,7 @@ function Settings1() {
     const [message, setMessage] = useState("");
     const [searchQuery, setSearchQuery] = useState(""); // State for search input
     const [studentDetails, setStudentDetails] = useState({});
-    const [user, setUser] = useState(null); 
+    const [user, setUser] = useState(null);
 
     useEffect(() => {
         fetch(`https://coursemanagementsystembackend-production.up.railway.app/api/v1/users/${id}`)
@@ -33,11 +42,9 @@ function Settings1() {
     useEffect(() => {
         const storedUser = JSON.parse(localStorage.getItem("user"));
         if (storedUser) {
-            setUser(storedUser);  // Set the user info to state
-            // Redirect to the dashboard with student ID in the URL (assuming the student ID is stored in user.id)
-            navigate(`/settings1/${id}`);
+            setUser(storedUser);
         } else {
-            navigate("/login");  // If no user, redirect to login page
+            navigate("/login"); // If no user, redirect to login page
         }
     }, [navigate, id]);
 
@@ -80,55 +87,95 @@ function Settings1() {
                 setLoading(false);
             });
     };
+
     const handleSearchChange = (e) => {
         setSearchQuery(e.target.value);
-      };
+    };
+
     return (
         <div className="dashboard-container">
-          <div className="sidebar">
-            <div className="logo">Coding Courses</div>
-           <ul>
-        <li><a href={`/admin-dashboard/${id}`}><FontAwesomeIcon icon={faHome} /> Home</a></li>
-          <li><a href={`/students-list/${id}`}><FontAwesomeIcon icon={faUsersCog} /> Users List</a></li>
-          <li><a href={`/course-list/${id}`}><FontAwesomeIcon icon={faGraduationCap} /> Courses List</a></li>
-          <li><a href={`/settings1/${id}`}><FontAwesomeIcon icon={faCog} /> Profile</a></li>
-          <li><a href={`/help1/${id}`}><FontAwesomeIcon icon={faQuestionCircle} /> Help</a></li>
-          <li><a href="/login"><FontAwesomeIcon icon={faSignOutAlt} /> Logout</a></li>
-        </ul>
-            <footer>
-                        <p>&copy; 2024 Uppada Giridhar. All Rights Reserved.</p>
-                        <div className="social-icons">
-                            <a href="https://www.facebook.com/sanjay.uppada.7/" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook"></i></a>
-                            <a href="https://x.com/giridhar_uppada" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i></a>
-                            <a href="https://www.linkedin.com/in/uppada-giridhar/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
-                            <a href="https://www.instagram.com/__sanjay__uppada__/?__pwa=1" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a>
-                        </div>
-                    </footer>
-          </div>
-    
-          <div className="main-content">
-            <header className="dashboard-header">
-              <div className="search-bar">
-                <input
-                  type="text"
-                  placeholder="Search courses..."
-                  value={searchQuery}
-                  onChange={handleSearchChange}
-                />
-              </div>
-              <h2>Welcome to the Admin Dashboard</h2>
-              <div className="username">
-              {user && (
+            <div className="sidebar">
+                <div className="logo">Coding Courses</div>
+                <ul>
+                    <li>
+                        <a href={`/admin-dashboard/${id}`}>
+                            <FontAwesomeIcon icon={faHome} /> Home
+                        </a>
+                    </li>
+                    <li>
+                        <a href={`/students-list/${id}`}>
+                            <FontAwesomeIcon icon={faUsersCog} /> Users List
+                        </a>
+                    </li>
+                    <li>
+                        <a href={`/course-list/${id}`}>
+                            <FontAwesomeIcon icon={faGraduationCap} /> Courses List
+                        </a>
+                    </li>
+                    <li>
+                        <a href={`/settings1/${id}`}>
+                            <FontAwesomeIcon icon={faCog} /> Profile
+                        </a>
+                    </li>
+                    <li>
+                        <a href={`/help1/${id}`}>
+                            <FontAwesomeIcon icon={faQuestionCircle} /> Help
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/login">
+                            <FontAwesomeIcon icon={faSignOutAlt} /> Logout
+                        </a>
+                    </li>
+                </ul>
+                <footer>
+                    <p>&copy; 2024 Uppada Giridhar. All Rights Reserved.</p>
+                    <div className="social-icons">
+                        <a href="https://www.facebook.com/sanjay.uppada.7/" target="_blank" rel="noopener noreferrer">
+                            <i className="fab fa-facebook"></i>
+                        </a>
+                        <a href="https://x.com/giridhar_uppada" target="_blank" rel="noopener noreferrer">
+                            <i className="fab fa-twitter"></i>
+                        </a>
+                        <a href="https://www.linkedin.com/in/uppada-giridhar/" target="_blank" rel="noopener noreferrer">
+                            <i className="fab fa-linkedin"></i>
+                        </a>
+                        <a
+                            href="https://www.instagram.com/__sanjay__uppada__/?__pwa=1"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <i className="fab fa-instagram"></i>
+                        </a>
+                    </div>
+                </footer>
+            </div>
+
+            <div className="main-content">
+                <header className="dashboard-header">
+                    <div className="search-bar">
+                        <input
+                            type="text"
+                            placeholder="Search courses..."
+                            value={searchQuery}
+                            onChange={handleSearchChange}
+                        />
+                    </div>
+                    <h2>Welcome to the Admin Dashboard</h2>
+                    <div className="username">
+                        {user && (
                             <div className="username">
-                                <p>Hello, <strong> {studentDetails.firstName} {studentDetails.lastName}</strong></p>  {/* Displaying username */}
+                                <p>
+                                    Hello, <strong>{studentDetails.firstName} {studentDetails.lastName}</strong>
+                                </p>
                             </div>
                         )}
-              </div>
-            </header>
-            <div className="settings-container">
-                        <button className="back-button" onClick={() => navigate(`/admin-dashboard/${id}`)}>
-                            <FontAwesomeIcon icon={faArrowLeft} /> Back
-                        </button>
+                    </div>
+                </header>
+                <div className="settings-container">
+                    <button className="back-button" onClick={() => navigate(`/admin-dashboard/${id}`)}>
+                        <FontAwesomeIcon icon={faArrowLeft} /> Back
+                    </button>
                     <h2>Settings</h2>
                     <form className="settings-form" onSubmit={handleSave}>
                         <h3>Update Profile</h3>
@@ -181,8 +228,7 @@ function Settings1() {
                 </div>
             </div>
         </div>
-            );
-        }
-        
+    );
+}
+
 export default Settings1;
-        
